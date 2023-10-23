7638df0b57bc639649e96d4137827eb5a677ad51
Sergio <sergiocarrascal000@gmail.com>
2023-10-23 11:01:28 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      
+    {/* Shift alt a Imágenes */}
+    <h3>Imagen del juego</h3>
+
+    {/* Palabra oculta */}
+    <h3>_ _</h3>
+    
+    </div>
+  )
+}
+
+export default App
