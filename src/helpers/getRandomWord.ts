14201d95a191f06dcf4e26dc9e233012f555d2d7
Sergio <sergiocarrascal000@gmail.com>
2023-10-24 22:47:05 +0200

    icon and words added

--- src/helpers/getRandomWord.ts
+++ src/helpers/getRandomWord.ts
@@ -1,9 +1,10 @@
 import { Word } from "./word.interface";
 
 const words: Word[] = [
-  { word: "Computadora", hint: "Es una maquina que procesa datos" },
-  { word: "Aguacate", hint: "Es una fruta" },
-  { word: "Animal", hint: "Es un ser vivo" },
+  { word: "Conatus", hint: "Esfuerzo de una cosa a permanecer o «perseverar en su ser», esfuerzo o potencia que identifica con la esencia misma de la cosa" },
+  { word: "Sustancia", hint: "Lo que es en sí y se concibe por sí mismo, es decir, aquello cuyo concepto no requiere el concepto de otra cosa, de la que debe formarse." },
+  { word: "Atributo", hint: "Lo que el intelecto percibe de una sustancia, como constituyendo su esencia." },
+  { word: "Modos", hint: "Los afectos de una sustancia, o lo que está en otra por la que también es concebida." },
 ];
 
 export function getRandomWord() {
